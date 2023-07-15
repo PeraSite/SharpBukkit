b5bc39656b6619a0a7442f36df6851f9d0ceb8ed
PeraSite <pretocki3@gmail.com>
2023-07-15 23:44:18 +0900

    feat(generator): Added helpful warning messages, Added array field support

--- SharpBukkit.Generator/Protocol/ProtocolGenerator.cs
+++ SharpBukkit.Generator/Protocol/ProtocolGenerator.cs
@@ -6,13 +6,14 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Serilog;
 using SharpBukkit.Generator.Protocol.Mapper;
 
 namespace SharpBukkit.Generator.Protocol;
 
 public static class ProtocolGenerator {
 	public static async Task Generate(string outputFolder, string protocolJson) {
-		Console.WriteLine("Parsing JSON...");
+		Log.Information("Parsing JSON...");
 		#region Parsing JSON
 		var json = JsonConvert.DeserializeObject<JObject>(protocolJson);
 		if (json == null) {
@@ -22,36 +23,36 @@ public static class ProtocolGenerator {
 
 		var packetTypes = new[] {PacketType.Handshaking, PacketType.Status, PacketType.Login, PacketType.Play};
 
-		Console.WriteLine("Generating codes...");
+		Log.Information("Generating codes...");
 		#region Generating code
 		var generatedCodeDict = new Dictionary<PacketInfo, string>();
 		foreach (PacketType packetType in packetTypes) {
 			var typeName = Enum.GetName(typeof(PacketType), packetType)!;
-			var typeNameLower = typeName.ToLower();
+			var packetName = typeName.ToLower();
 
-			if (!json.TryGetValue(typeNameLower, out var namespaceJson)) {
-				throw new Exception($"Can't find packet type: {typeNameLower}");
+			if (!json.TryGetValue(packetName, out var namespaceJson)) {
+				throw new Exception($"Can't find packet type: {packetName}");
 			}
 
-			var toClientJson = namespaceJson["toClient"]!.ToObject<JObject>();
-			var toServerJson = namespaceJson["toServer"]!.ToObject<JObject>();
+			var toClientJson = namespaceJson["toClient"]!.ToObject<JObject>()!;
+			var toServerJson = namespaceJson["toServer"]!.ToObject<JObject>()!;
 
 			IEnumerable<PacketInfo> clientPackets = ReadPacketStructs(packetType, BoundType.Client, toClientJson).ToList();
 			IEnumerable<PacketInfo> serverPackets = ReadPacketStructs(packetType, BoundType.Server, toServerJson).ToList();
 
-			Console.WriteLine($"* {typeNameLower}");
+			Log.Information("* {PacketType}", packetName);
 
 			if (clientPackets.Any()) {
-				Console.WriteLine("   # toClient");
+				Log.Information("   # toClient");
 				foreach (var item in clientPackets) {
-					Console.WriteLine($"     - {item}");
+					Log.Information("     - {Packet}", item);
 				}
 			}
 
 			if (serverPackets.Any()) {
-				Console.WriteLine("   # toServer");
+				Log.Information("   # toServer");
 				foreach (var item in serverPackets) {
-					Console.WriteLine($"     - {item}");
+					Log.Information("     - {Packet}", item);
 				}
 			}
 
@@ -61,7 +62,7 @@ public static class ProtocolGenerator {
 		}
 #endregion
 
-		Console.WriteLine("Writing to files...");
+		Log.Information("Writing to files...");
 		#region Writing to file
 		await Task.WhenAll(generatedCodeDict.Select(pair => {
 			var (packet, content) = pair;
@@ -71,13 +72,13 @@ public static class ProtocolGenerator {
 
 			Directory.CreateDirectory(Path.Combine(outputFolder, directoryName));
 			var output = Path.Combine(outputFolder, directoryName, fileName);
-			Console.WriteLine($"* {directoryName} - {fileName}");
+			Log.Information("* {DirectoryName} - {FileName}", directoryName, fileName);
 
 			return File.WriteAllTextAsync(output, content);
 		}));
   #endregion
 
-		Console.WriteLine("Generating packet registry...");
+		Log.Information("Generating packet registry...");
 		#region Generating packet registry
 		var registryFilePath = Path.Combine(outputFolder, "PacketRegistry.g.cs");
 		await File.WriteAllTextAsync(registryFilePath, $$"""
@@ -116,23 +117,25 @@ public class PacketRegistry : IPacketRegistry {
 
 """);
   #endregion
+
+		Log.Information("Done!");
 	}
 
 	private static IEnumerable<PacketInfo> ReadPacketStructs(PacketType packetType, BoundType bound, JObject json) {
-		var types = json["types"].ToObject<JObject>();
-		var meta = types["packet"]!.ToObject<JArray>();
+		var types = json["types"]!.ToObject<JObject>()!;
+		var meta = types["packet"]!.ToObject<JArray>()!;
 
 		var metaKey = meta[0].ToObject<string>();
 		if (metaKey != "container")
 			throw new InvalidOperationException("No meta key match!");
 
-		var metaArray = meta[1].ToObject<JArray>();
-		var idDict = metaArray[0]["type"]!.ToObject<JArray>()[1]["mappings"]!.ToObject<Dictionary<string, string>>();
-		var paramsDict = metaArray[1]["type"]!.ToObject<JArray>()[1]["fields"]!.ToObject<Dictionary<string, string>>();
+		var metaArray = meta[1].ToObject<JArray>()!;
+		var idDict = metaArray[0]["type"]!.ToObject<JArray>()![1]["mappings"]!.ToObject<Dictionary<string, string>>()!;
+		var paramsDict = metaArray[1]["type"]!.ToObject<JArray>()![1]["fields"]!.ToObject<Dictionary<string, string>>()!;
 
 		foreach (var pair in types) {
 			string packetName = pair.Key;
-			JToken packetJson = pair.Value;
+			JToken packetJson = pair.Value!;
 
 			const string packetPrefix = "packet_";
 			if (!packetName.StartsWith(packetPrefix))
@@ -142,8 +145,8 @@ public class PacketRegistry : IPacketRegistry {
 			var packetIdStr = idDict.SingleOrDefault(s => s.Value == packetKey).Key;
 			var packetId = Convert.ToByte(packetIdStr.Replace("0x", ""), 16);
 
-			var fieldListJson = packetJson.ToObject<JArray>()[1];
-			List<FieldInfo> fieldList = ParsePacketFields(fieldListJson);
+			var fieldListJson = packetJson.ToObject<JArray>()![1];
+			List<FieldInfo> fieldList = ParsePacketFields(fieldListJson, packetName);
 
 			yield return new PacketInfo {
 				Id = packetId,
@@ -155,11 +158,11 @@ public class PacketRegistry : IPacketRegistry {
 		}
 	}
 
-	private static List<FieldInfo> ParsePacketFields(JToken fieldListJson) {
+	private static List<FieldInfo> ParsePacketFields(JToken fieldListJson, string packetName) {
 		var fieldList = new List<FieldInfo>();
 
 		foreach (var fieldJson in fieldListJson) {
-			var actualFieldName = fieldJson["name"]!.ToObject<string>();
+			var actualFieldName = fieldJson["name"]!.ToObject<string>()!;
 			var fieldName = ToTitleCase(actualFieldName);
 			var fieldType = fieldJson["type"];
 
@@ -167,6 +170,11 @@ public class PacketRegistry : IPacketRegistry {
 				case JValue: {
 					var simpleFieldType = fieldType.ToObject<string>();
 					var nativeFieldType = FieldMapping.GetNative(simpleFieldType, actualFieldName);
+
+					if (nativeFieldType == null) {
+						Log.Warning("Unknown field type {FieldType} for {FieldName}: {Raw}", simpleFieldType, actualFieldName, fieldJson.ToString());
+						break;
+					}
 
 					fieldList.Add(new FieldInfo {
 						ActualType = simpleFieldType,
@@ -178,9 +186,14 @@ public class PacketRegistry : IPacketRegistry {
 				case JArray typeArray: {
 					var typeName = typeArray[0].ToObject<string>();
 					switch (typeName) {
-						case "buffer":
+						case "buffer": {
 							const string actualElementType = "buffer";
 							var nativeElementType = FieldMapping.GetNative(actualElementType, actualFieldName);
+
+							if (nativeElementType == null) {
+								Log.Warning("[{Packet}] Unknown field type {FieldType} for {FieldName}: {Raw}", packetName, actualElementType, actualFieldName, fieldJson.ToString());
+								break;
+							}
 
 							fieldList.Add(new FieldInfo {
 								ActualType = actualElementType,
@@ -188,8 +201,35 @@ public class PacketRegistry : IPacketRegistry {
 								Name = fieldName
 							});
 							break;
+						}
+						case "array": {
+							var elementTypeJson = typeArray[1]["type"];
+							switch (elementTypeJson) {
+								case JValue valueJson: {
+									var actualElementType = $"{valueJson.ToObject<string>()}[]";
+									var nativeElementType = FieldMapping.GetNative(actualElementType, actualFieldName);
+
+									if (nativeElementType == null) {
+										Log.Warning("[{Packet}] Unknown field type {FieldType} for {FieldName}: {Raw}", packetName, actualElementType, actualFieldName, fieldJson.ToString());
+										break;
+									}
+
+									fieldList.Add(new FieldInfo {
+										ActualType = actualElementType,
+										NativeType = nativeElementType,
+										Name = fieldName
+									});
+									break;
+								}
+								case JArray:
+									//TODO: Handle array of complex types
+									break;
+							}
+							break;
+						}
+						// TODO: handles more types
 						default:
-							Console.WriteLine($"[ERROR] Unhandled type: {fieldType}");
+							Log.Warning("[{Packet}] Unknown field type {FieldType} for {FieldName}: {Raw}", packetName, typeName, actualFieldName, fieldJson.ToString());
 							break;
 					}
 					break;
@@ -242,7 +282,7 @@ public record {{className}} : IPacket {
 
 	private static string ToTitleCase(string text) {
 		if (string.IsNullOrWhiteSpace(text))
-			return null;
+			return string.Empty;
 		var bld = new StringBuilder();
 		foreach (var part in text.Split('_')) {
 			var big = part.Substring(0, 1)
@@ -256,7 +296,7 @@ public record {{className}} : IPacket {
 
 	private static string ToCamelCase(string text) {
 		if (string.IsNullOrWhiteSpace(text))
-			return null;
+			return string.Empty;
 		var bld = new StringBuilder();
 		foreach (var part in text.Split('_')) {
 			var big = part[..1].ToUpperInvariant() + part[1..];
